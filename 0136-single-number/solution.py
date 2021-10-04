51a0e8cb5b39e114a0db8c5b8146800d915769b1
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2021-10-04 03:25:43 +0000

    Sync LeetCode submission Runtime - 166 ms (14.49%), Memory - 16.7 MB (100.00%)

---
+++ 0136-single-number/solution.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
+        

8738c969b02f4d68e766b67abfa2d3f07b901ec1
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2021-12-28 20:23:33 +0000

    Sync LeetCode submission - Valid Anagram (python3)

---
+++ problems/valid_anagram/solution.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_count = Counter(s)
+        t_count = Counter(t)
+        
+        return True if s_count == t_count else False
+        

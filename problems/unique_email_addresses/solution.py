d72420bbae9ff43d70c493f315373d36468a1ed2
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-01-01 00:11:22 +0000

    Sync LeetCode submission - Unique Email Addresses (python3)

--- problems/unique_email_addresses/solution.py
+++ problems/unique_email_addresses/solution.py
@@ -1,10 +1,13 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        local_names = set()
+        unique_emails = set()
+        
         for email in emails:
-            local_name = email.split('@')[0]
-            domain_name = email.split('@')[1]
-            local_name = local_name.split('+')[0]
-            local_name = local_name.replace(".", "")
-            local_names.add(local_name+'@'+domain_name)
-        return len(local_names)
+            local, domain = email.split('@')
+            
+            local = local.split('+')[0].replace('.', '')
+            
+            unique_emails.add(local + '@' + domain)
+            
+        return len(unique_emails)
+        

56d3875028eedea273733e93004e85fdc03a5ce4
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-01-12 04:17:27 +0000

    Sync LeetCode submission - Closest Binary Search Tree Value (python3)

---
+++ problems/closest_binary_search_tree_value/solution.py
@@ -0,0 +1,16 @@
+# Approach 1 - Recursive Inorder + Linear search, O(N) time
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        
+        def inorder(r: TreeNode):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+        
+        return min(inorder(root), key=lambda x: abs(x-target))
+        

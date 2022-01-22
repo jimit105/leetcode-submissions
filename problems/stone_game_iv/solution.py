d4f756216cf0d9be9f253e766978bba24c814fae
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-01-22 05:09:30 +0000

    Sync LeetCode submission - Stone Game IV (python3)

---
+++ problems/stone_game_iv/solution.py
@@ -0,0 +1,17 @@
+# Approach 2 - DP
+
+# Time: O(N*sqrt(N))
+# Space: O(N)
+
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        dp = [False] * (n+1)
+        
+        for i in range(1, n+1):
+            for k in range(1, int(i**0.5)+1):
+                if dp[i - k*k] == False:
+                    dp[i] = True
+                    break
+                    
+        return dp[n]
+        

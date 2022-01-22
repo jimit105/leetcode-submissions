69f36cf8eae6769db5126900011189f575f6ea9e
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-01-22 21:15:27 +0000

    Sync LeetCode submission - Fibonacci Number (python3)

--- problems/fibonacci_number/solution.py
+++ problems/fibonacci_number/solution.py
@@ -1,21 +1,16 @@
-# Approach 4 - Iterative Bottom-Up Approach
+# Approach 3 - Top-Down Approach using Memoization
 
-# Time: O(n)
-# Space: O(1)
+# Time: O(N)
+# Space: O(N)
 
 class Solution:
+    cache = {0: 0, 1: 1}
+    
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
+        if n in self.cache:
+            return self.cache[n]
         
-        current = 0
-        prev1 = 1
-        prev2 = 0
+        self.cache[n] = self.fib(n-1) + self.fib(n-2)
         
-        for i in range(2, n+1):
-            current = prev1 + prev2
-            prev2 = prev1
-            prev1 = current
-            
-        return current
+        return self.cache[n]
         

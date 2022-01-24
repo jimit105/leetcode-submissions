c65a819d47b9bc216b1426cfda0ba6874239ba14
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-01-24 00:14:55 +0000

    Sync LeetCode submission Runtime - 32 ms (76.42%), Memory - 14.3 MB (100.00%)

--- 0094-binary-tree-inorder-traversal/solution.py
+++ 0094-binary-tree-inorder-traversal/solution.py
@@ -6,14 +6,5 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        output = []
-        
-        def dfs(node):
-            if node:
-                dfs(node.left)
-                output.append(node.val)
-                dfs(node.right)
-                
-        dfs(root)
-        return output
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
         

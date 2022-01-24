48c0d28f79664fbc5e37bbf64482912d722db49b
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-01-24 05:55:18 +0000

    Sync LeetCode submission Runtime - 57 ms (5.88%), Memory - 13.9 MB (100.00%)

---
+++ 0198-house-robber/solution.py
@@ -0,0 +1,23 @@
+# Approach 3 - Optimized Dynamic Programming
+
+# Time: O(N)
+# Space: O(1)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        N = len(nums)
+        
+        rob_next_plus_one = 0
+        rob_next = nums[N - 1]
+        
+        for i in range(N - 2, -1, -1):
+            current = max(rob_next, rob_next_plus_one + nums[i])
+            
+            rob_next_plus_one = rob_next
+            rob_next = current
+            
+        return rob_next
+        

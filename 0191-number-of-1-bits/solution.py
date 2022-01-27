bbb91e76f2a7f933c2e8d220332d3f4da4fd222c
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-01-27 05:56:46 +0000

    Sync LeetCode submission Runtime - 32 ms (78.26%), Memory - 13.8 MB (100.00%)

---
+++ 0191-number-of-1-bits/solution.py
@@ -0,0 +1,10 @@
+# Approach 2 - Bit Manipulation Trick
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n != 0:
+            count += 1
+            n &= (n - 1)
+        return count
+        

03c6aafa5cd998f6dfbd9f59fe4cbf22710e7622
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-01-28 22:50:16 +0000

    Sync LeetCode submission Runtime - 60 ms (5.61%), Memory - 16.4 MB (100.00%)

---
+++ 0098-validate-binary-search-tree/solution.py
@@ -0,0 +1,28 @@
+# Approach 1 - Recursive Traversal with Valid Range
+
+# Time: O(N)
+# Space: O(N)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+import math
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def validate(node, low=-math.inf, high=math.inf):
+            if not node:
+                return True
+            
+            if node.val <= low or node.val >= high:
+                return False
+            
+            return validate(node.right, node.val, high) and validate(node.left, low, node.val)
+        
+        return validate(root)
+        

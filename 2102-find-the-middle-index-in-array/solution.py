a2b87c795a931f4a3f73bc5d4b8054fad02b344b
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-02-10 18:45:09 +0000

    Sync LeetCode submission Runtime - 69 ms (5.26%), Memory - 13.9 MB (100.00%)

---
+++ 2102-find-the-middle-index-in-array/solution.py
@@ -0,0 +1,18 @@
+# Approach 1 - Prefix Sum
+
+# Time: O(N)
+# Space: O(1)
+
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        S = sum(nums)
+        left_sum = 0
+        
+        for i, x in enumerate(nums):
+            if left_sum == (S - left_sum - x):
+                return i
+            left_sum += x
+            
+        return -1
+        
+        

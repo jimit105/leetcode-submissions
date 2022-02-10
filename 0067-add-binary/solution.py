2e6786013536c90b1b147c6dd400ef767e7eec8f
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-02-10 19:09:17 +0000

    Sync LeetCode submission Runtime - 28 ms (95.91%), Memory - 13.9 MB (100.00%)

--- 0067-add-binary/solution.py
+++ 0067-add-binary/solution.py
@@ -1,4 +1,14 @@
+# Approach 2 - Bit Manipulation
+
+# Time: O(N+M)
+# Space: O(max(N, M))
+
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, 2) + int(b, 2))[2:]
+        x, y = int(a, 2), int(b, 2)
+        
+        while y:
+            x, y = x ^ y, (x & y) << 1
+            
+        return '{0:b}'.format(x)
         

6affe9b2ae388366bef5681c0ce26a52b93c1e30
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-02-22 01:22:29 +0000

    Sync LeetCode submission - String to Integer (atoi) (python3)

--- problems/string_to_integer_(atoi)/solution.py
+++ problems/string_to_integer_(atoi)/solution.py
@@ -1,38 +1,28 @@
+# Time: O(N)
+
 class Solution:
     def myAtoi(self, s: str) -> int:
         sign, result = 1, 0
         s = s.lstrip()
         
-        idx = 0
-        
-        if len(s) == 0:
+        if not s:
             return 0
         
-        if s[0] == '-':
+        idx = 0
+        
+        if s[idx] == '-':
             sign = -1
             idx += 1
-            
-        elif s[0] == '+':
+        elif s[idx] == '+':
             idx += 1
             
-        int_s = ''
+        int_str = ''
         for i in range(idx, len(s)):
             if s[i].isdigit():
-                int_s += s[i]
+                int_str += s[i]
             else:
                 break
                 
-        int_s = int_s if len(int_s) else "0"
+        result = sign * int(int_str) if int_str else 0
         
-        output = int(int_s) * sign
-        
-        if output < -1 * pow(2, 31):
-            return -1 * pow(2, 31)
-        
-        if output > pow(2, 31) - 1:
-            return pow(2, 31) -1
-        
-        return output
-        
-        
-        
+        return max(min(result, 2**31 - 1), -1 * 2**31)

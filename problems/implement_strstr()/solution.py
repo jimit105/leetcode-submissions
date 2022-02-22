4687c907af4c341481fb1484c77c0ec5964c2fe1
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-02-22 19:09:15 +0000

    Sync LeetCode submission - Implement strStr() (python3)

--- problems/implement_strstr()/solution.py
+++ problems/implement_strstr()/solution.py
@@ -1,4 +1,18 @@
+# Approach - Sliding Window
+
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        if not needle:
+            return 0
+        
+        n, k = len(haystack), len(needle)
+        
+        if haystack[0 : k] == needle:
+            return 0
+        
+        for i in range(k, n):
+            if haystack[i - k + 1 : i + 1] == needle:
+                return i - k + 1
+            
+        return -1
         

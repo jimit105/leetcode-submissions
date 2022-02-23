a2015a67ad63518ce932414a3aa96f5a45a65b3e
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-02-23 01:54:31 +0000

    Sync LeetCode submission - Reorder Data in Log Files (python3)

--- problems/reorder_data_in_log_files/solution.py
+++ problems/reorder_data_in_log_files/solution.py
@@ -1,11 +1,14 @@
+# Approach 2 - Sorting by Keys
+
+# Time: O(m * n logn), n = number of logs, m = max length of single log
+
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         
         def get_key(log):
-            id_, rest = log.split(' ', maxsplit=1)
-            
-            # (0, rest, id_) for letter logs
-            # (1, ) for digit logs
-            return (0, rest, id_) if rest[0].isalpha() else (1, )
+            id_, rest = log.split(' ', maxsplit = 1)
+            return (0, rest, id_) if rest[0].isalpha() else (1,)
         
-        return sorted(logs, key=get_key)
+        return sorted(logs, key = get_key)
+            
+        

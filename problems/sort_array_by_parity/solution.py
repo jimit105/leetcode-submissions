95912747ed57eb7fde2a8e5d724e830f730f9410
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-05-02 04:06:14 +0000

    Sync LeetCode submission - Sort Array By Parity (python3)

--- problems/sort_array_by_parity/solution.py
+++ problems/sort_array_by_parity/solution.py
@@ -1,4 +1,8 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        nums.sort(key=lambda x: x%2)
-        return nums
+        evens = list(filter(lambda x: x % 2 == 0, nums))
+        odds = list(filter(lambda x: x % 2 != 0, nums))
+        
+        evens.extend(odds)
+        return evens
+        

1023bac79147b1d3c1c02a63ce81be9933890d8c
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-06-19 07:08:28 +0000

    Sync LeetCode submission Runtime - 39 ms (75.58%), Memory - 13.9 MB (100.00%)

--- 1603-running-sum-of-1d-array/solution.py
+++ 1603-running-sum-of-1d-array/solution.py
@@ -1,11 +1,10 @@
-# Appraoch 2 - Using Input Array for Output
+# Approach 2 - Using Input Array for Output
 
-# Time: O(N)
+# Time: O(n)
 # Space: O(1)
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        
         for i in range(1, len(nums)):
             nums[i] += nums[i - 1]
             

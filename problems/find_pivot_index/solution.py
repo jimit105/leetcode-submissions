dfb72840dabc85e582c092e8f7c6633383009d11
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-06-19 07:13:54 +0000

    Sync LeetCode submission - Find Pivot Index (python3)

--- problems/find_pivot_index/solution.py
+++ problems/find_pivot_index/solution.py
@@ -1,16 +1,17 @@
 # Approach 1 - Prefix Sum
 
-# Time: O(N)
+# Time: O(n)
 # Space: O(1)
 
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        S = sum(nums)
+        s = sum(nums)
         left_sum = 0
         
         for i, x in enumerate(nums):
-            if left_sum == (S - left_sum - x):
+            if left_sum == (s - left_sum - x):
                 return i
             left_sum += x
             
         return -1
+        

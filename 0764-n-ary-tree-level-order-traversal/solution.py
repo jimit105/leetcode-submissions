1b25a16a3e6c057560aa257ce880a30f4d984275
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-09-05 19:24:29 +0000

    Sync LeetCode submission Runtime - 69 ms (5.14%), Memory - 16.1 MB (100.00%)

---
+++ 0764-n-ary-tree-level-order-traversal/solution.py
@@ -0,0 +1,33 @@
+# Approach 1: Breadth-first Search using a Queue
+    
+# Time: O(N)
+# Space: O(N)
+
+from collections import deque
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if root is None:
+            return []
+        
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                queue.extend(node.children)
+            result.append(level)
+            
+        return result
+        

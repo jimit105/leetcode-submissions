64e13633ddbd39fba1a1b9da0be01f4ab4bf6b62
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-09-06 22:39:51 +0000

    Sync LeetCode submission Runtime - 39 ms (32.13%), Memory - 14 MB (100.00%)

---
+++ 0832-binary-tree-pruning/solution.py
@@ -0,0 +1,32 @@
+# Approach 1: Recursion
+
+# Time: O(N)
+# Space: O(N)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def contains_one(node: TreeNode) -> bool:
+            if not node:
+                return False
+            
+            left_contains_one = contains_one(node.left)
+            right_contains_one = contains_one(node.right)
+            
+            if not left_contains_one:
+                node.left = None
+                
+            if not right_contains_one:
+                node.right = None
+                
+            return node.val or left_contains_one or right_contains_one
+        
+        return root if contains_one(root) else None
+        

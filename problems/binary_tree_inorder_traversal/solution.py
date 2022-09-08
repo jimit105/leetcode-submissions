d332b5f5a2f7ef73c59c352f719795249393db3f
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-09-08 03:52:47 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (python3)

--- problems/binary_tree_inorder_traversal/solution.py
+++ problems/binary_tree_inorder_traversal/solution.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []

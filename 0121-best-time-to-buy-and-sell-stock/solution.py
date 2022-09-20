4faf18dda093486898ad50d8cc11065393176a27
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-09-20 02:26:00 +0000

    Sync LeetCode submission Runtime - 2340 ms (5.00%), Memory - 25 MB (99.66%)

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,22 +1,18 @@
+# Approach 2: One Pass
+
+# Time: O(N)
+# Space: O(1)
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
-        buy = prices[0]
+        min_price = float('inf')
         max_profit = 0
         
-        for i in range(1, len(prices)):
-            profit = prices[i] - buy
-            
-            if profit > max_profit:
-                max_profit = profit
-                
-            # if the current prices is less than buy, then the max_profit will be from buying current stock
-            if buy > prices[i]:
-                buy = prices[i]
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
                 
         return max_profit
-    
-    
-# Time: O(n) - single pass
-# Space: O(1)
+        

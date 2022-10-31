b101a82bc9837bc18da9d870aeedd07823a81606
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-10-31 23:51:56 +0000

    Sync LeetCode submission - Toeplitz Matrix (python3)

--- problems/toeplitz_matrix/solution.py
+++ problems/toeplitz_matrix/solution.py
@@ -1,21 +1,13 @@
-# Approach 1: Group by Category
+# Approach 2: Compare with Top-Left Neighbor
 
 # Time: O(M * N)
-# Space: O(M + N)
+# Space: O(1)
 
-
-# two coordinates are on the same diagonal if and only if r1 - c1 == r2 - c2
+# Every element belongs to some diagonal, and it's previous element (if it exists) is it's top-left neighbor. Thus, for the square (r, c), we only need to check r == 0 OR c == 0 OR matrix[r-1][c-1] == matrix[r][c]
 
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        groups = {}
-        
-        for r, row in enumerate(matrix):
-            for c, val in enumerate(row):
-                if (r - c) not in groups:
-                    groups[r-c] = val
-                elif groups[r-c] != val:
-                    return False
-                
-        return True
+        return all(r == 0 or c == 0 or matrix[r-1][c-1] == val
+                   for r, row in enumerate(matrix)
+                   for c, val in enumerate(row))
         

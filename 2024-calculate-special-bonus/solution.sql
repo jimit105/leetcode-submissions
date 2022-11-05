766077ec28215e3ec7df6127beea474431b0c8b3
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-11-05 02:13:30 +0000

    Sync LeetCode submission Runtime - 565 ms (66.91%), Memory - 0B (100.00%)

--- 2024-calculate-special-bonus/solution.sql
+++ 2024-calculate-special-bonus/solution.sql
@@ -1,3 +1,3 @@
-SELECT employee_id, IF(employee_id % 2 AND name NOT LIKE 'M%', salary, 0) as 'bonus'
+SELECT employee_id, IF(employee_id % 2 != 0 AND name NOT LIKE 'M%', salary, 0) as 'bonus'
 FROM Employees
 ORDER BY employee_id;

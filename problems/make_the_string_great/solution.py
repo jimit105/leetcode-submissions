9604086f3236c0416b47922cc8d948d62a394b60
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-11-08 05:40:55 +0000

    Sync LeetCode submission - Make The String Great (python3)

---
+++ problems/make_the_string_great/solution.py
@@ -0,0 +1,17 @@
+# Approach 3: Stack
+    
+# Time: O(n)
+# Space: O(n)
+
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        
+        for curr_char in list(s):
+            if stack and abs(ord(curr_char) - ord(stack[-1])) == 32:
+                stack.pop()
+            else:
+                stack.append(curr_char)
+                
+        return ''.join(stack)
+        

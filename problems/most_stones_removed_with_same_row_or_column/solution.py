d79076f61ca9882893576a25dcca286419a0741a
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-11-14 01:31:48 +0000

    Sync LeetCode submission - Most Stones Removed with Same Row or Column (python3)

---
+++ problems/most_stones_removed_with_same_row_or_column/solution.py
@@ -0,0 +1,41 @@
+# Approach 2: Disjoint Set Union
+
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
+        rank = [1] * n
+        parent = [i for i in range(n)]
+        
+        def find(i):
+            while i != parent[i]:
+                parent[i] = i = parent[parent[i]]
+            return i
+        
+        def union(i, j):
+            i, j = find(i), find(j)
+            if i == j:
+                return 0
+            
+            if rank[i] < rank[j]:
+                i, j = j, i
+            rank[i] += rank[j]
+            parent[j] = parent[i]
+            return 1
+        
+        
+        rows, cols = {}, {}
+        removed = 0
+        
+        for i, (row, col) in enumerate(stones):
+            if row in rows:
+                removed += union(i, rows[row])
+            else:
+                rows[row] = i
+            
+            if col in cols:
+                removed += union(i, cols[col])
+            else:
+                cols[col] = i
+                
+        return removed
+        

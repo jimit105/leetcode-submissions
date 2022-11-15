a0aa4176a7210d637a10b583e4c36139e5e00eab
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-11-15 21:14:16 +0000

    Sync LeetCode submission - Count Complete Tree Nodes (python3)

---
+++ problems/count_complete_tree_nodes/solution.py
@@ -0,0 +1,16 @@
+# Approach 1: Linear Time
+    
+# Time: O(N)
+# Space: O(d) = O(log N), d = tree depth
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right) if root else 0
+        

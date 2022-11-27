f6a667e97867e8044bff7e3c4f29fa5186d2cc1c
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-11-27 04:35:40 +0000

    Sync LeetCode submission - Arithmetic Slices II - Subsequence (python3)

---
+++ problems/arithmetic_slices_ii_-_subsequence/solution.py
@@ -0,0 +1,17 @@
+# Approach: Dynamic Programming
+
+# https://leetcode.com/problems/arithmetic-slices-ii-subsequence/discuss/2851807/Python-two-counting-schemes
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n = len(nums)
+        cnt = [defaultdict(int) for _ in range(n)]
+        
+        subseq = 0
+        for i, a in enumerate(nums):
+            for j, b in enumerate(nums[:i]):
+                cnt[i][a-b] += cnt[j][a-b] + 1
+                subseq += cnt[j][a-b]
+                
+        return subseq
+        

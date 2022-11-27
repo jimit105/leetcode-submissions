62a09a18c081c637bab49b3ddc8b143dfce940fa
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-11-27 03:59:45 +0000

    Sync LeetCode submission - Arithmetic Slices (python3)

--- problems/arithmetic_slices/solution.py
+++ problems/arithmetic_slices/solution.py
@@ -1,17 +1,19 @@
-# Approach 4 - Dynamic Programming
-
+# Approach 5: Constant Space Dynamic Programming
+    
 # Time: O(n)
-# Space: O(n)
+# Space: O(1)
 
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        dp = [0] * len(nums)
-        total = 0
+        dp = 0
+        sum_ = 0
         
         for i in range(2, len(nums)):
-            if nums[i] - nums[i-1]  == nums[i-1] - nums[i - 2]:
-                dp[i] = 1 + dp[i - 1]
-                total += dp[i]
+            if (nums[i] - nums[i-1]) == (nums[i-1] - nums[i-2]):
+                dp += 1
+                sum_ += dp
+            else:
+                dp = 0
                 
-        return total
+        return sum_
         

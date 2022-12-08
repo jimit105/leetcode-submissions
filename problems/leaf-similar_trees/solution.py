67f468fb3064bcf2045fc6bb410f8b708d0991c4
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2022-12-08 00:57:44 +0000

    Sync LeetCode submission - Leaf-Similar Trees (python3)

---
+++ problems/leaf-similar_trees/solution.py
@@ -0,0 +1,22 @@
+# Approach 1: Depth First Search
+
+# Time: O(T1 + T2), T1, T2 = length of the given trees
+# Space: O(T1 + T2), space used in storing leaf values
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if node:
+                if not node.left and not node.right:
+                    yield node.val
+                yield from dfs(node.left)
+                yield from dfs(node.right)
+
+        return list(dfs(root1)) == list(dfs(root2))

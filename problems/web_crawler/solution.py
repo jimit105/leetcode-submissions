ec3bcaf84cd39b29711ddbf28ceee256a94f584b
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2023-03-26 23:38:21 +0000

    Sync LeetCode submission - Web Crawler (python3)

---
+++ problems/web_crawler/solution.py
@@ -0,0 +1,29 @@
+# Approach 1: Depth First Search
+
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        def get_hostname(url):
+            return url.split('/')[2]
+
+        start_hostname = get_hostname(startUrl)
+        visited = set()
+
+        def dfs(url):
+            visited.add(url)
+            for next_url in htmlParser.getUrls(url):
+                if get_hostname(next_url) == start_hostname and next_url not in visited:
+                    dfs(next_url)
+
+        dfs(startUrl)
+        return visited

bba0eb2d465ec685a5d72d8674afad9a5a57faca
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2023-06-06 06:03:52 +0000

    Sync LeetCode submission - Can Make Arithmetic Progression From Sequence (python3)

---
+++ problems/can_make_arithmetic_progression_from_sequence/solution.py
@@ -0,0 +1,24 @@
+# Approach 2: Set
+
+# Time: O(n)
+# Space: O(n)
+
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        min_value, max_value = min(arr), max(arr)
+        n = len(arr)
+
+        if max_value - min_value == 0:
+            return True
+        if (max_value - min_value) % (n - 1):
+            return False
+
+        diff = (max_value - min_value) // (n - 1)
+        number_set = set()
+
+        for a in arr:
+            if (a - min_value) % diff:
+                return False
+            number_set.add(a)
+
+        return len(number_set) == n

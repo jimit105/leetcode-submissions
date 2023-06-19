e85ab324fad383f967cb3ea45a00f1d2ed97335e
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2023-06-19 05:46:46 +0000

    Sync LeetCode submission Runtime - 61 ms (6.20%), Memory - 16.4 MB (61.55%)

---
+++ 1833-find-the-highest-altitude/solution.py
@@ -0,0 +1,15 @@
+# Approach 1: Prefix Sum
+
+# Time: O(N)
+# Space: O(1)
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        curr_altitude = 0
+        highest_point = curr_altitude
+
+        for altitude_gain in gain:
+            curr_altitude += altitude_gain
+            highest_point = max(highest_point, curr_altitude)
+
+        return highest_point

25f81c35cf35613ade35ddd5128650257f4a2e78
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2023-06-22 22:38:01 +0000

    Sync LeetCode submission Runtime - 689 ms (32.02%), Memory - 23.7 MB (54.98%)

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.py
@@ -0,0 +1,16 @@
+# Approach 2: Space-Optimized Dynamic Programming
+
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        hold, free = -prices[0], 0
+
+        for i in range(1, n):
+            tmp = hold
+            hold = max(hold, free - prices[i])
+            free = max(free, tmp + prices[i] - fee)
+
+        return free

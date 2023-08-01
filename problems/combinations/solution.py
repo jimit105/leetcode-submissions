1b93b90807d80d2f85f6165f1d3a5fbc758d8d6d
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2023-08-01 00:18:58 +0000

    Sync LeetCode submission - Combinations (python3)

--- problems/combinations/solution.py
+++ problems/combinations/solution.py
@@ -1,17 +1,25 @@
-# Approach 1 - Backtracking
+# Approach 1: Backtracking
+
+# Space: O(k)
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def backtrack(first = 1, curr = []):
+        def backtrack(curr, first_num):
             if len(curr) == k:
-                output.append(curr[:])
-                
-            for i in range(first, n + 1):
-                curr.append(i)
-                backtrack(i + 1, curr)
+                ans.append(curr[:])
+                return
+
+            need = k - len(curr)
+            remain = n - first_num + 1
+            available = remain - need
+
+            for num in range(first_num, first_num + available + 1):
+                curr.append(num)
+                backtrack(curr, num + 1)
                 curr.pop()
-                
-        output = []
-        backtrack()
-        return output
-        
+
+            return
+
+        ans = []
+        backtrack([], 1)
+        return ans

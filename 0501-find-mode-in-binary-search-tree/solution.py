6661605c5c657ad5f6635748b46566e585d9a9b2
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2023-11-01 07:36:27 +0000

    Sync LeetCode submission Runtime - 55 ms (14.43%), Memory - 20.8 MB (5.13%)

---
+++ 0501-find-mode-in-binary-search-tree/solution.py
@@ -0,0 +1,35 @@
+# Approach 1: Count Frequency with Hash Map (DFS)
+
+# Time: O(n)
+# Space: O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import defaultdict
+
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        def dfs(node, counter):
+            if not node:
+                return
+
+            counter[node.val] += 1
+            dfs(node.left, counter)
+            dfs(node.right, counter)
+
+        counter = defaultdict(int)
+        dfs(root, counter)
+        max_freq = max(counter.values())
+
+        ans = []
+        for key in counter:
+            if counter[key] == max_freq:
+                ans.append(key)
+
+        return ans
+        

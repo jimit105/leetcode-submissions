58d99a01dc9835867a931506b9fdec7b654fbdb5
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-11-02 15:46:31 +0000

    Sync LeetCode submission Runtime - 3 ms (14.87%), Memory - 16.6 MB (18.77%)

---
+++ 2580-circular-sentence/solution.py
@@ -0,0 +1,12 @@
+# Approach 2: Space-optimized
+
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        for i in range(len(sentence)):
+            if sentence[i] == ' ' and sentence[i-1] != sentence[i+1]:
+                return False
+        return sentence[0] == sentence[-1]
+        

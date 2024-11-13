44bbbd8dcbc92bbbe2539ea14bc91bddc9d46dbc
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-11-13 14:12:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.6 MB (62.67%)

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/solution.py
@@ -0,0 +1,20 @@
+# Approach: Two Pointer
+
+# Time: O(n log n)
+# Space: O(n)
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        count = 0
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            if (nums[left] + nums[right]) < target:
+                count += right - left
+                left += 1
+            else:
+                right -= 1
+
+        return count
+        

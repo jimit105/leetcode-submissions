9144455bb51be031f3e11a1735bd2eec45cb5c44
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-14 20:28:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.1 MB (25.16%)

--- 0226-invert-binary-tree/solution.py
+++ 0226-invert-binary-tree/solution.py
@@ -1,3 +1,8 @@
+# Approach 1:  Recursive
+
+# Time: O(n)
+# Space: O(n)
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,8 +12,10 @@
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root is None:
+        if not root:
             return None
-        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
-        return root
         
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+        return root
+

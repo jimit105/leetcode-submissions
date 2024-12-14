898e2a8229e215daa6d546ea57d3a7b22d12d7d4
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-14 18:47:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.6 MB (7.97%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -6,10 +6,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = dict() # value: index
-        
-        for i in range(len(nums)):
-            complement = target - nums[i]
+
+        for i, num in enumerate(nums):
+            complement = target - num
             if complement in hashmap:
                 return [i, hashmap[complement]]
-            
-            hashmap[nums[i]] = i
+
+            hashmap[num] = i
+
+        return []
+        

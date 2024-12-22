bce1109816b2e984f6b17693cfc52fdc061a15b1
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-22 18:56:53 +0000

    Sync LeetCode submission Runtime - 34 ms (40.18%), Memory - 18.6 MB (52.61%)

--- 0966-binary-subarrays-with-sum/solution.py
+++ 0966-binary-subarrays-with-sum/solution.py
@@ -1,22 +1,28 @@
-# Approach 1: Prefix Sum
+# Approach 2: Sliding Window
 
 # Time: O(n)
-# Space: O(n)
+# Space: O(1)
 
 class Solution:
-    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        total_count = 0
-        curr_sum = 0
-        sum_freq = {0 : 1} # {prefix_sum : frequency}
+    # Helper function to count the number of subarrays with sum at most the given goal
+    def sliding_window_at_most(self, nums: List[int], goal: int) -> int:
+        start, curr_sum, total_count = 0, 0, 0
 
-        for num in nums:
-            curr_sum += num
-            if curr_sum - goal in sum_freq:
-                total_count += sum_freq[curr_sum - goal]
-            
-            sum_freq[curr_sum] = sum_freq.get(curr_sum, 0) + 1
+        for end in range(len(nums)):
+            curr_sum += nums[end]
+
+            # Adjust the window by moving the start pointer to the right
+            # until the sum becomes less than or equal to the goal
+            while start <= end and curr_sum > goal:
+                curr_sum -= nums[start]
+                start += 1
+
+            # Update the total count by adding the length of the current subarray
+            total_count += end - start + 1
 
         return total_count
 
 
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        return self.sliding_window_at_most(nums, goal) - self.sliding_window_at_most(nums, goal - 1)
         

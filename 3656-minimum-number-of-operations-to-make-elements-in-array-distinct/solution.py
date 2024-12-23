2d76a3f6b8ec649b2f2bebd88b022e36bb07ee40
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-23 01:50:10 +0000

    Sync LeetCode submission Runtime - 7 ms (100.00%), Memory - 17.6 MB (100.00%)

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/solution.py
@@ -0,0 +1,13 @@
+# Time: O(n^2)
+# Space: O(n)
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        count = 0
+
+        while len(set(nums)) != len(nums):
+            nums = nums[3: ]
+            count += 1
+
+        return count
+        

48aa0e7be71a9c2922f823476f09e87f7132a3ef
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-23 02:08:07 +0000

    Sync LeetCode submission Runtime - 8 ms (22.94%), Memory - 20.9 MB (5.81%)

--- 0543-diameter-of-binary-tree/solution.py
+++ 0543-diameter-of-binary-tree/solution.py
@@ -1,25 +1,31 @@
+# Approach 1: Depth First Search
+
+# Time: O(n)
+# Space: O(n)
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        diameter = 0 
-        
-        def find_longest_path(node):
-            nonlocal diameter
+        diameter = 0
+
+        def longest_path(node):
             if not node:
                 return 0
-            
-            left_path = find_longest_path(node.left)
-            right_path = find_longest_path(node.right)
-            
-            diameter = max(diameter, left_path+right_path)
-            
-            return max(left_path, right_path)+1
-        
-        find_longest_path(root)
+            nonlocal diameter
+
+            left_path = longest_path(node.left)
+            right_path = longest_path(node.right)
+
+            diameter = max(diameter, left_path + right_path)
+
+            return max(left_path, right_path) + 1
+
+        longest_path(root)
         return diameter
         

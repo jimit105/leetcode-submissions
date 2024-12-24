4a0422331fe4040e16c8b755f153b3228579ccaa
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-24 23:27:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (6.97%)

--- 0088-merge-sorted-array/solution.py
+++ 0088-merge-sorted-array/solution.py
@@ -1,6 +1,6 @@
-# Approach 3 - Three Pointers (Start from the End)
+# Approach 3: Three Pointers (Start from the end)
 
-# Time: O(m+n)
+# Time: O(n + m)
 # Space: O(1)
 
 class Solution:
@@ -8,10 +8,13 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # Set p1 and p2 to point to the end of their respective arrays.
         p1 = m - 1
         p2 = n - 1
-        
-        for p in range(n+m-1, -1, -1):
+
+        # And move p backward through the array, each time writing
+        # the smallest value pointed at by p1 or p2.
+        for p in range(n + m - 1, -1, -1):
             if p2 < 0:
                 break
             if p1 >= 0 and nums1[p1] > nums2[p2]:
@@ -20,3 +23,4 @@ class Solution:
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
+        

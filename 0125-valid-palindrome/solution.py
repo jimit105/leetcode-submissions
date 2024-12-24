01429a218dbcbd40892fa5ca1ba89464476880d9
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-24 23:44:43 +0000

    Sync LeetCode submission Runtime - 7 ms (81.31%), Memory - 18 MB (32.06%)

---
+++ 0125-valid-palindrome/solution.py
@@ -0,0 +1,23 @@
+# Approach 2: Two Pointers
+
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+        

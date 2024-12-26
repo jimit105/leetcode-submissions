bf2d188eb39bc23d9ba13758b1d117ab1c0473ab
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-26 02:34:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (5.01%)

---
+++ 0314-binary-tree-vertical-order-traversal/solution.py
@@ -0,0 +1,36 @@
+# Approach 2: BFS without sorting
+
+# Time: O(n)
+# Space: O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import defaultdict, deque
+
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        columnTable = defaultdict(list)
+        min_column = max_column = 0
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, column = queue.popleft()
+
+            if node is not None:
+                columnTable[column].append(node.val)
+                min_column = min(min_column, column)
+                max_column = max(max_column, column)
+
+                queue.append([node.left, column - 1])
+                queue.append([node.right, column + 1])
+
+        return [columnTable[x] for x in range(min_column, max_column + 1)]
+        

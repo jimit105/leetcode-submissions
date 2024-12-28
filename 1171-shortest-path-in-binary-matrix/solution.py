3b5aa34bda3f6a3214a0dd9a02c646315b88df5c
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-28 23:07:54 +0000

    Sync LeetCode submission Runtime - 159 ms (75.48%), Memory - 18.1 MB (64.41%)

--- 1171-shortest-path-in-binary-matrix/solution.py
+++ 1171-shortest-path-in-binary-matrix/solution.py
@@ -1,9 +1,10 @@
-# Approach 1: Breadth First Search, overwrite input
+# Approach 1: Breadth-first Search (BFS), Overwriting Input
 
-# Time: O(N)
-# Space: O(N)
+# n = no. of cells
+# Time: O(n)
+# Space: O(n)
 
-import collections
+from collections import deque
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
@@ -15,17 +16,19 @@ class Solution:
             for row_diff, col_diff in directions:
                 new_row = row + row_diff
                 new_col = col + col_diff
-                if not(0 <= new_row <= max_row and 0 <= new_col <= max_col):
+
+                if not (0 <= new_row <= max_row and 0 <= new_col <= max_col):
                     continue
                 if grid[new_row][new_col] != 0:
                     continue
-                
                 yield (new_row, new_col)
 
+        # Check that first and last cells are open
         if grid[0][0] != 0 or grid[max_row][max_col] != 0:
             return -1
 
-        queue = collections.deque()
+        # Set up BFS
+        queue = deque()
         queue.append((0, 0))
         grid[0][0] = 1
 
@@ -34,6 +37,7 @@ class Solution:
             distance = grid[row][col]
             if (row, col) == (max_row, max_col):
                 return distance
+
             for neighbor_row, neighbor_col in get_neighbors(row, col):
                 grid[neighbor_row][neighbor_col] = distance + 1
                 queue.append((neighbor_row, neighbor_col))

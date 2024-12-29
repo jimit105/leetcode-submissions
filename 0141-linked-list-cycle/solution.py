e4d0d1f47bd82ed12a9cf63e92a1dcd458a3be96
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-29 19:59:25 +0000

    Sync LeetCode submission Runtime - 46 ms (55.96%), Memory - 19.7 MB (25.14%)

--- 0141-linked-list-cycle/solution.py
+++ 0141-linked-list-cycle/solution.py
@@ -1,6 +1,7 @@
-# Approach 2 - Floyd's Cycle Finding Algorithm
+# Approach 2: Floyd's Cycle Finding Algorithm (Fast and Slow Pointer)
 
-# Time: O(n), Space: O(1)
+# Time: O(n)
+# Space: O(1)
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -14,11 +15,12 @@ class Solution:
             return False
         slow = head
         fast = head.next
-        
+
         while slow != fast:
             if fast is None or fast.next is None:
                 return False
             slow = slow.next
             fast = fast.next.next
-            
+        
         return True
+        

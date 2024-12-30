700f2be718e4ca84f3617f4dabc3930fd5381872
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-30 06:54:10 +0000

    Sync LeetCode submission Runtime - 2 ms (12.32%), Memory - 18.1 MB (7.43%)

--- 0078-subsets/solution.py
+++ 0078-subsets/solution.py
@@ -1,19 +1,21 @@
-# Approach 3 - Lexicographic (Binary Sorted) Subsets
+# Approach 2: Backtracking
 
-# Time: O(N * 2^N)
-# Space: O(N * 2^N)    
+# Time: O(n * 2^n)
+# Space: O(n)
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        output = []
-        
-        for i in range(2**n, 2**(n+1)):
-            # generate bitmask, from 0..00 to 1..11
-            bitmask = bin(i)[3:]
-            
-            # append subset corresponding to that bitmask
-            output.append([nums[j] for j in range(n) if bitmask[j] == '1'])
-            
-        return output
+        self.output = []
+        self.n, self.k = len(nums), None
+        for self.k in range(self.n + 1):
+            self.backtrack(0, [], nums)
+        return self.output
+
+    def backtrack(self, first, curr, nums):
+        if len(curr) == self.k:
+            self.output.append(curr[:])
+        for i in range(first, self.n):
+            curr.append(nums[i])
+            self.backtrack(i + 1, curr, nums)
+            curr.pop()
         

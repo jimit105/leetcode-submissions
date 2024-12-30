392d1131ea47480d259bcbf612fd2cde3a091ebb
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-30 01:53:27 +0000

    Sync LeetCode submission Runtime - 44 ms (54.52%), Memory - 28.7 MB (45.55%)

---
+++ 0041-first-missing-positive/solution.py
@@ -0,0 +1,27 @@
+# Approach 3: Cycle Sort
+
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        i = 0
+        while i < n:
+            correct_idx = nums[i] - 1
+            if 0 < nums[i] <= n and nums[i] != nums[correct_idx]:
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+            else:
+                i += 1
+
+        # Iterate through nums
+        # return smallest missing positive integer
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+
+        # If all elements are at the correct index
+        # the smallest missing positive number is n + 1
+        return n + 1
+        

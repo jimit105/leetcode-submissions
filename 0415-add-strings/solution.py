eec6888ac2f519d1d8cbb1e9f37c65fe8caec13a
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-30 07:33:27 +0000

    Sync LeetCode submission Runtime - 3 ms (90.88%), Memory - 18.3 MB (6.96%)

--- 0415-add-strings/solution.py
+++ 0415-add-strings/solution.py
@@ -1,29 +1,27 @@
-# Approach 1 - Ordinals + Elementary Math
+# Approach 1: Elementary Math
 
-# Time: O(max(N1, N2))
-# Space: O(max(N1, N2))
+# Time: O(max(n1, n2))
+# Space: O(max(n1, n2))
 
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         res = []
+
+        p1 = len(num1) - 1
+        p2 = len(num2) - 1
         carry = 0
-        
-        p1, p2 = len(num1) - 1, len(num2) - 1
-        
+
         while p1 >= 0 or p2 >= 0:
             x1 = ord(num1[p1]) - ord('0') if p1 >= 0 else 0
             x2 = ord(num2[p2]) - ord('0') if p2 >= 0 else 0
-            
             value = (x1 + x2 + carry) % 10
             carry = (x1 + x2 + carry) // 10
-            
             res.append(value)
             p1 -= 1
             p2 -= 1
-            
+
         if carry:
             res.append(carry)
-            
+
         return ''.join(str(x) for x in res[::-1])
-        
         

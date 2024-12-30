b29f1986b68ecbb4a9fc25212b83ea9a3abf892c
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-30 04:42:36 +0000

    Sync LeetCode submission Runtime - 1 ms (81.58%), Memory - 17.8 MB (15.20%)

--- 0002-add-two-numbers/solution.py
+++ 0002-add-two-numbers/solution.py
@@ -1,25 +1,33 @@
+# Approach 1: Elementary Math
+
+# Time: O(max(n, m))
+# Space: O(1)
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        result = ListNode(0)
-        result_list = result
+        dummy_head = ListNode(0)
+        curr = dummy_head
         carry = 0
-        
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            sum_val = val1 + val2 + carry
-            carry = sum_val // 10
-            ans = sum_val % 10
+
+        while l1 != None or l2 != None or carry != 0:
+            l1val = l1.val if l1 else 0
+            l2val = l2.val if l2 else 0
+
+            column_sum = l1val + l2val + carry
+            carry = column_sum // 10
             
-            result_list.next = ListNode(ans)
-            result_list = result_list.next
-            
+            new_node = ListNode(column_sum % 10)
+            curr.next = new_node
+            curr = new_node
+
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            
-        return result.next
+
+        return dummy_head.next
+

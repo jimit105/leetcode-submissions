53fc10e7cfad6564eca1543a3204884dcec7a1b0
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2024-12-31 22:59:07 +0000

    Sync LeetCode submission Runtime - 3 ms (38.30%), Memory - 17.7 MB (11.70%)

--- 0777-toeplitz-matrix/solution.py
+++ 0777-toeplitz-matrix/solution.py
@@ -1,13 +1,12 @@
-# Approach 2: Compare with Top-Left Neighbor
+# Approach 2: Compare With Top-Left Neighbor
 
-# Time: O(M * N)
+# m = no. of rows, n = no. of cols
+# Time: O(m * n)
 # Space: O(1)
-
-# Every element belongs to some diagonal, and it's previous element (if it exists) is it's top-left neighbor. Thus, for the square (r, c), we only need to check r == 0 OR c == 0 OR matrix[r-1][c-1] == matrix[r][c]
 
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        return all(r == 0 or c == 0 or matrix[r-1][c-1] == val
-                   for r, row in enumerate(matrix)
-                   for c, val in enumerate(row))
-        
+        return all(r == 0 or c == 0 or matrix[r - 1][c - 1] == val
+                    for r, row in enumerate(matrix)
+                    for c, val in enumerate(row))
+

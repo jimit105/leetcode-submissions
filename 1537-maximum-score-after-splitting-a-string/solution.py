ac41b3da8f64ab818361e6f5681542772914222d
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-01 19:46:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (16.51%)

---
+++ 1537-maximum-score-after-splitting-a-string/solution.py
@@ -0,0 +1,21 @@
+# Approach 2: Count Left Zeros and Right Ones
+
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def maxScore(self, s: str) -> int:
+        ones = s.count('1')
+        zeros = 0
+        ans = 0
+
+        for i in range(len(s) - 1):
+            if s[i] == '1':
+                ones -= 1
+            else:
+                zeros += 1
+
+            ans = max(ans, ones + zeros)
+
+        return ans
+        

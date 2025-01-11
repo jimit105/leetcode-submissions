d4d232c3656cec3a62f29f41407b3b6fb40fb145
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-11 06:08:29 +0000

    Sync LeetCode submission Runtime - 51 ms (25.11%), Memory - 18.2 MB (8.81%)

---
+++ 1502-construct-k-palindrome-strings/solution.py
@@ -0,0 +1,24 @@
+# Approach 1: Count Odd Frequencies
+
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+        if len(s) == k:
+            return True
+
+        freq = [0] * 26
+        odd_count = 0
+
+        for char in s:
+            freq[ord(char) - ord('a')] += 1
+
+        for count in freq:
+            if count % 2 == 1:
+                odd_count += 1
+
+        return odd_count <= k
+        

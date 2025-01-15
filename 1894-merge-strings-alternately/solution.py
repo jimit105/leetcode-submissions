86771fb2d542ccc645409d6d6857813db39b78ae
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-15 05:08:50 +0000

    Sync LeetCode submission Runtime - 37 ms (55.34%), Memory - 17.5 MB (53.43%)

--- 1894-merge-strings-alternately/solution.py
+++ 1894-merge-strings-alternately/solution.py
@@ -5,12 +5,13 @@
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        result = ''
-        max_length = max(len(word1), len(word2))
-        for i in range(max_length):
+        result = []
+        n = max(len(word1), len(word2))
+
+        for i in range(n):
             if i < len(word1):
                 result += word1[i]
             if i < len(word2):
                 result += word2[i]
 
-        return result
+        return ''.join(result)

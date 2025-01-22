0b0d971d4df6959fd860134eb673092fdba049e6
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-22 07:07:30 +0000

    Sync LeetCode submission Runtime - 11 ms (53.86%), Memory - 31.5 MB (42.56%)

--- 0217-contains-duplicate/solution.py
+++ 0217-contains-duplicate/solution.py
@@ -1,10 +1,16 @@
+# Approach 3: Hash Set
+
+# Time: O(n)
+# Space: O(n)
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         visited = set()
-        
+
         for num in nums:
             if num in visited:
                 return True
-            else:
-                visited.add(num)
+            visited.add(num)
+
         return False
+        

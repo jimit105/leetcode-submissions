a70e1b4177cd0191c6a1a1434546c7dc13c2797d
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-23 21:09:02 +0000

    Sync LeetCode submission Runtime - 86 ms (37.55%), Memory - 31.5 MB (85.17%)

--- 0053-maximum-subarray/solution.py
+++ 0053-maximum-subarray/solution.py
@@ -1,14 +1,15 @@
-# Approach 2 - Dynamic Programming
+# Approach 2: Dynamic Programming
 
-# Time: O(N)
+# Time: O(n)
 # Space: O(1)
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         current_subarray = max_subarray = nums[0]
-        
+
         for num in nums[1:]:
-            current_subarray = max(current_subarray + num, num)
+            current_subarray = max(num, current_subarray + num)
             max_subarray = max(max_subarray, current_subarray)
-            
+
         return max_subarray
+        

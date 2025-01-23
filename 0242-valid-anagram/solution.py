ad083ec7e250bb3346bc352ee311767641a2be78
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-23 21:46:02 +0000

    Sync LeetCode submission Runtime - 11 ms (74.27%), Memory - 18 MB (25.35%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,8 +1,18 @@
-from collections import Counter
+# Approach 2: Frequency Counter
+
+# Time: O(n)
+# Space: O(1)
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_count = Counter(s)
-        t_count = Counter(t)
-        
-        return True if s_count == t_count else False
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+
+        return all(c == 0 for c in count)
         

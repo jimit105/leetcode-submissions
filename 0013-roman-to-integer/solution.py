70743c3edfb307613039e6f48532c3923f83ae27
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-23 21:23:30 +0000

    Sync LeetCode submission Runtime - 4 ms (65.62%), Memory - 17.8 MB (26.42%)

--- 0013-roman-to-integer/solution.py
+++ 0013-roman-to-integer/solution.py
@@ -1,4 +1,7 @@
-# Approach 2 - Left-to-Right Pass Improved
+# Approach 2: Left to Right Pass Improved
+
+# Time: O(n) = O(1), n = length of input
+# Space: O(1)
 
 values = {
     "I": 1,
@@ -10,26 +13,26 @@ values = {
     "M": 1000,
     "IV": 4,
     "IX": 9,
-    "XL": 40, 
+    "XL": 40,
     "XC": 90,
     "CD": 400,
-    "CM": 900
+    "CM": 900,
 }
+
 
 class Solution:
     def romanToInt(self, s: str) -> int:
         total = 0
         i = 0
-        
+
         while i < len(s):
-            
-            # subtractive case
+            # Subtractive case
             if i < len(s) - 1 and s[i : i + 2] in values:
                 total += values[s[i : i + 2]]
                 i += 2
-                
             else:
                 total += values[s[i]]
                 i += 1
-        
+
         return total
+

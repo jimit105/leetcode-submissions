228edae709c5ff633bc6248131f7e4d266ca56ab
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-23 19:48:40 +0000

    Sync LeetCode submission Runtime - 4 ms (16.12%), Memory - 17.9 MB (18.55%)

--- 0008-string-to-integer-atoi/solution.py
+++ 0008-string-to-integer-atoi/solution.py
@@ -1,28 +1,35 @@
-# Time: O(N)
+# Time: O(n)
+# Space: O(n)
 
 class Solution:
     def myAtoi(self, s: str) -> int:
         sign, result = 1, 0
+
+        # Whitespace
         s = s.lstrip()
-        
+
         if not s:
             return 0
-        
+
         idx = 0
-        
+
+        # Signedness
         if s[idx] == '-':
             sign = -1
             idx += 1
         elif s[idx] == '+':
             idx += 1
-            
+
+        # Conversion
         int_str = ''
         for i in range(idx, len(s)):
             if s[i].isdigit():
                 int_str += s[i]
             else:
                 break
-                
+
         result = sign * int(int_str) if int_str else 0
-        
-        return max(min(result, 2**31 - 1), -1 * 2**31)
+
+        # Rounding
+        return max(min(result, 2 ** 31 - 1), -1 * 2 ** 31)
+

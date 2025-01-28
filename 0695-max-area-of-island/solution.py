2f4bff844655f8688208cf38708ed5b8866094d7
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-28 04:15:33 +0000

    Sync LeetCode submission Runtime - 32 ms (27.64%), Memory - 19.3 MB (19.48%)

--- 0695-max-area-of-island/solution.py
+++ 0695-max-area-of-island/solution.py
@@ -1,20 +1,21 @@
-# Approach 1 - Depth-First Search (Recursive)
+# Approach 1: Depth First Search (Recursive)
 
-# Time: O(R*C)
-# Space: O(R*C)
+# Time: O(m * n)
+# Space: O(m * n)
 
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         seen = set()
-        
+
         def area(r, c):
             if not (0 <= r < len(grid) and 0 <= c < len(grid[0]) and (r, c) not in seen and grid[r][c]):
                 return 0
-            
+
             seen.add((r, c))
-            
-            return (1 + area(r-1, c) + area(r+1, c) + area(r, c+1) + area(r, c-1))
-        
-        return max(area(r, c) for r in range(len(grid)) for c in range(len(grid[0])))
-        
+
+            return 1 + area(r + 1, c) + area(r - 1, c) + area(r, c + 1) + area(r, c - 1)
+
+        return max(area(r, c) 
+                    for r in range(len(grid))
+                    for c in range(len(grid[0])))
         

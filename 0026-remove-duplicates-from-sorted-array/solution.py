ce8472fc58110ea313dc5843e72bfb496a36ba7b
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-01-30 22:17:45 +0000

    Sync LeetCode submission Runtime - 3 ms (49.29%), Memory - 18.8 MB (74.47%)

--- 0026-remove-duplicates-from-sorted-array/solution.py
+++ 0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,10 +1,17 @@
+# Approach 1: Two indexes approach
+
+# Time: O(n)
+# Space: O(1)
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        i = 0
-        for j in range(len(nums)):
-            if nums[j] != nums[i]:
-                i += 1
-                nums[i] = nums[j]
-        return i+1
+        size = len(nums)
+        insert_idx = 1
+
+        for i in range(1, size):
+            if nums[i - 1] != nums[i]:
+                nums[insert_idx] = nums[i]
+                insert_idx += 1
+
+        return insert_idx
+        

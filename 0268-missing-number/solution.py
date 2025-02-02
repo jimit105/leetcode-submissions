8307a790841ae00959c6c3097a1c4fb347723e7b
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-02-02 23:58:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.5 MB (77.72%)

---
+++ 0268-missing-number/solution.py
@@ -0,0 +1,11 @@
+# Approach 4: Math
+
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum

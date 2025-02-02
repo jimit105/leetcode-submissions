25eb16c739918bd9eac0fb5554c90255402da419
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-02-02 22:27:08 +0000

    Sync LeetCode submission Runtime - 4 ms (49.84%), Memory - 18.8 MB (8.03%)

--- 0167-two-sum-ii---input-array-is-sorted/solution.py
+++ 0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -1,22 +1,20 @@
-# Approach 1 - Two Pointers
+# Approach 1: Two Pointers
 
-# Time: O(N)
+# Time: O(n)
 # Space: O(1)
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        n = len(numbers)
-        left, right = 0, n - 1
-        
-        while left < right:
-            sum_ = numbers[left] + numbers[right]
-            
-            if sum_ == target:
-                return [left+1, right+1]
-            elif sum_ < target:
-                left += 1
+        low, high = 0, len(numbers) - 1
+
+        while low < high:
+            total = numbers[low] + numbers[high]
+            if total == target:
+                return [low + 1, high + 1]
+            elif total < target:
+                low += 1
             else:
-                right -= 1
-                
+                high -= 1
+
         return [-1, -1]
         

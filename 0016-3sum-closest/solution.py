0f0492a7a5a8143dd2b6f2d2480eb6f85f05eea0
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-02-02 23:54:29 +0000

    Sync LeetCode submission Runtime - 323 ms (74.68%), Memory - 17.7 MB (63.13%)

--- 0016-3sum-closest/solution.py
+++ 0016-3sum-closest/solution.py
@@ -1,28 +1,25 @@
-# Approach 1 - Two Pointers
+# Approach 1: Two Pointers
 
-# Time: O(n^2)
+# Time: O(n ^ 2)
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         diff = float('inf')
         nums.sort()
-        
+
         for i in range(len(nums)):
             lo, hi = i + 1, len(nums) - 1
-            
+
             while lo < hi:
                 total = nums[i] + nums[lo] + nums[hi]
                 if abs(target - total) < abs(diff):
                     diff = target - total
-                    
                 if total < target:
                     lo += 1
                 else:
                     hi -= 1
-                    
-                if diff == 0:
-                    break
-                    
+            
+            if diff == 0:
+                break
+
         return target - diff
-                                                  
-        

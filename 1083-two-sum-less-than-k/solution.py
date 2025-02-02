22bd48c4f12891c191e422c490abd943afa16055
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-02-02 23:42:40 +0000

    Sync LeetCode submission Runtime - 3 ms (46.65%), Memory - 17.8 MB (50.89%)

--- 1083-two-sum-less-than-k/solution.py
+++ 1083-two-sum-less-than-k/solution.py
@@ -1,21 +1,20 @@
-# Approach 2 - Two Pointers
+# Approach 2: Two Pointers
 
 # Time: O(n log n)
 
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
         nums.sort()
-        
-        result = -1
+        answer = -1
         left, right = 0, len(nums) - 1
-        
+
         while left < right:
             total = nums[left] + nums[right]
             if total < k:
-                result = max(result, total)
+                answer = max(answer, total)
                 left += 1
             else:
                 right -= 1
-                
-        return result
+
+        return answer
         

49d0860b6d1a3ed95ed1b696d72eda619ba4c2a3
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-02-27 05:01:54 +0000

    Sync LeetCode submission Runtime - 846 ms (61.26%), Memory - 18.1 MB (74.83%)

---
+++ 0905-length-of-longest-fibonacci-subsequence/solution.py
@@ -0,0 +1,25 @@
+# Approach 1: Brute Force
+
+# M = largest number in arr
+# Time: O(n^2 log M)
+# Space: O(n)
+
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        num_set = set(arr)
+        max_len = 0
+        n = len(arr)
+
+        for start in range(n):
+            for next in range(start + 1, n):
+                prev = arr[next]
+                curr = arr[start] + arr[next]
+                curr_len = 2
+
+                while curr in num_set:
+                    prev, curr = curr, curr + prev
+                    curr_len += 1
+                    max_len = max(max_len, curr_len)
+
+        return max_len
+        

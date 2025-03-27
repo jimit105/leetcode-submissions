4b224e78f2eacf39cb440e469d0b0a4d975bffab
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-03-27 06:06:19 +0000

    Sync LeetCode submission Runtime - 115 ms (24.26%), Memory - 34.6 MB (24.26%)

---
+++ 2888-minimum-index-of-a-valid-split/solution.py
@@ -0,0 +1,26 @@
+# Approach 1: Hash Map
+
+# Time: O(n)
+# Space: O(n)
+
+from collections import defaultdict
+
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        first_map = defaultdict(int)
+        second_map = defaultdict(int)
+        n = len(nums)
+
+        for num in nums:
+            second_map[num] += 1
+
+        for index in range(n):
+            num = nums[index]
+            second_map[num] -= 1
+            first_map[num] += 1
+
+            if first_map[num] * 2 > index + 1 and second_map[num] * 2 > n - index - 1:
+                return index
+
+        return -1
+        

61b4c46e150b225fdd507a33d899edf7914d3319
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-04-06 23:51:50 +0000

    Sync LeetCode submission Runtime - 131 ms (98.87%), Memory - 18.8 MB (17.14%)

---
+++ 0368-largest-divisible-subset/solution.py
@@ -0,0 +1,14 @@
+# Approach 1: Dynamic Programming
+
+# Time: O(n^2)
+# Space: O(n^2)
+
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        subsets = {-1: set()}
+
+        for num in sorted(nums):
+            subsets[num] = max([subsets[k] for k in subsets if num % k == 0], key = len) | {num}
+
+        return list(max(subsets.values(), key=len))
+        

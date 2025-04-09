20dcde54c155535e9c39445e5df74974a215e09a
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-04-09 05:35:28 +0000

    Sync LeetCode submission Runtime - 61 ms (86.32%), Memory - 17.8 MB (53.07%)

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/solution.py
@@ -0,0 +1,16 @@
+# Approach 1: Hash Set
+
+# Time: O(n)
+# Space: O(n)
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        st = set()
+        for num in nums:
+            if num < k:
+                return -1
+            elif num > k:
+                st.add(num)
+
+        return len(st)
+        

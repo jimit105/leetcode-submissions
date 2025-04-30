1ab0b3b3a91c871490ece85e51487d44fccd81f7
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-04-30 05:49:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (83.23%)

--- 1421-find-numbers-with-even-number-of-digits/solution.py
+++ 1421-find-numbers-with-even-number-of-digits/solution.py
@@ -1,14 +1,17 @@
-class Solution(object):
-    def findNumbers(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count = 0
-        
+# Approach 2: Convert to String
+
+# N = len(nums), M = max(nums)
+# Time: O(N * log M)
+# Space: O(log M)
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        result = 0
+
         for num in nums:
-            if len(str(num)) % 2 == 0:
-                count += 1
-                
-        return count
+            len_ = len(str(num))
+            if len_ % 2 == 0:
+                result += 1
+
+        return result
         

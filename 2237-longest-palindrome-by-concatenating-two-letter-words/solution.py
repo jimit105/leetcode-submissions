11882be4e48eaad825fb6807173c123ac349e71a
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-05-25 01:27:17 +0000

    Sync LeetCode submission Runtime - 54 ms (66.43%), Memory - 35.7 MB (12.27%)

--- 2237-longest-palindrome-by-concatenating-two-letter-words/solution.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/solution.py
@@ -5,24 +5,21 @@ from collections import Counter
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         count = Counter(words)
-        answer = 0
+        ans = 0
         central = False
-        
+
         for word, word_count in count.items():
-            
-            # if the word is palindrome
+            # If the word is palindrome
             if word[0] == word[1]:
-                if word_count %2 == 0:
-                    answer += word_count
+                if word_count % 2 == 0:
+                    ans += word_count
                 else:
-                    answer += word_count - 1
+                    ans += word_count - 1
                     central = True
-                    
-            else:
-                answer += min(word_count, count[word[1] + word[0]])
-                
+            elif word[0] < word[1]:
+                ans += 2 * min(word_count, count[word[1] + word[0]])
+
         if central:
-            answer += 1
-            
-        return 2 * answer
-        
+            ans += 1
+
+        return 2 * ans

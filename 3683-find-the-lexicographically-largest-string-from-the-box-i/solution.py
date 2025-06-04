b0bdfb95717dceb079ba97fa61e47ec9084252f2
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-06-04 06:24:38 +0000

    Sync LeetCode submission Runtime - 23 ms (29.55%), Memory - 18.1 MB (32.58%)

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/solution.py
@@ -0,0 +1,17 @@
+# Approach 1: Enumeration
+
+# Time: O(n^2)
+# Space: O(1)
+
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+
+        n = len(word)
+        res = ""
+        for i in range(n):
+            res = max(res, word[i : min(i + n - numFriends + 1, n)])
+
+        return res
+        

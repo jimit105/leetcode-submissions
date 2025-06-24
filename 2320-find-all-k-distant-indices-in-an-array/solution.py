c32b06339167de4c7997b2df09e955fc6bb1a708
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-06-24 04:25:49 +0000

    Sync LeetCode submission Runtime - 4 ms (73.83%), Memory - 17.9 MB (68.60%)

---
+++ 2320-find-all-k-distant-indices-in-an-array/solution.py
@@ -0,0 +1,20 @@
+# Approach 2: One-time Traversal
+
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        res = []
+        r = 0
+        n = len(nums)
+
+        for j in range(n):
+            if nums[j] == key:
+                l = max(r, j - k)
+                r = min(n - 1, j + k) + 1
+                for i in range(l, r):
+                    res.append(i)
+
+        return res
+        

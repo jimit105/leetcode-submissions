36d69ceaef78e3aee5e770150660399d680b7986
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2025-07-27 22:43:30 +0000

    Sync LeetCode submission Runtime - 63 ms (61.84%), Memory - 18.5 MB (94.55%)

---
+++ 0527-word-abbreviation/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def wordsAbbreviation(self, words: List[str]) -> List[str]:
+        def abbrev(word, i = 0):
+            if (len(word) - i <= 3):
+                return word
+            return word[:i+1] + str(len(word) - i - 2) + word[-1]
+
+        N = len(words)
+        ans = list(map(abbrev, words))
+        prefix = [0] * N
+
+        for i in range(N):
+            while True:
+                dupes = set()
+                for j in range(i+1, N):
+                    if ans[i] == ans[j]:
+                        dupes.add(j)
+
+                if not dupes:
+                    break
+                dupes.add(i)
+                for k in dupes:
+                    prefix[k] += 1
+                    ans[k] = abbrev(words[k], prefix[k])
+
+        return ans

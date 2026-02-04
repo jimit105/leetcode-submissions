08791efce82cafabb7cebe5589ae59744ca3dd0e
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2026-02-04 00:27:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (20.13%)

---
+++ 3952-trionic-array-i/solution.py
@@ -0,0 +1,24 @@
+# Approach 1: Brute Force
+
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        n = len(nums)
+        i = 1
+
+        while i < n and nums[i - 1] < nums[i]:
+            i += 1
+        p = i - 1
+
+        while i < n and nums[i - 1] > nums[i]:
+            i += 1
+        q = i - 1
+
+        while i < n and nums[i - 1] < nums[i]:
+            i += 1
+        flag = i - 1
+
+        return (p != 0) and (q != p) and (flag == n - 1 and flag != q)
+        

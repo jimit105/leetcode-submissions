179fe81f67a5b01fa07aff3c4d698ef17cb8e86b
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2026-02-12 07:23:42 +0000

    Sync LeetCode submission Runtime - 2635 ms (57.05%), Memory - 19.4 MB (20.27%)

---
+++ 4055-longest-balanced-substring-i/solution.py
@@ -0,0 +1,21 @@
+# Approach: Enumeration
+
+# Time: O(C * n^2), C = size of character set, n = len(s)
+# Space: O(C)
+
+from collections import defaultdict
+
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        n = len(s)
+        res = 0
+
+        for i in range(n):
+            cnt = defaultdict(int)
+            for j in range(i, n):
+                cnt[s[j]] += 1
+                if len(set(cnt.values())) == 1:
+                    res = max(res, j - i + 1)
+
+        return res
+

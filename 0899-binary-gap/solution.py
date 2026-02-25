f094524ff63b53eead1211178e86bec039c4d570
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2026-02-25 05:46:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (73.83%)

---
+++ 0899-binary-gap/solution.py
@@ -0,0 +1,14 @@
+# Approach 1: Store Indexes
+
+# Time: O(log n)
+# Space: O(log n)
+
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        A = [i for i in range(32) if (n >> i) & 1]
+
+        if len(A) < 2:
+            return 0
+        
+        return max(A[i + 1] - A[i] for i in range(len(A) - 1))
+        

e7d9b76c9c36bae663a5c0f54c02f92e61d36f16
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2026-05-12 03:57:40 +0000

    Sync LeetCode submission Runtime - 59 ms (43.56%), Memory - 55.6 MB (30.69%)

---
+++ 1784-minimum-initial-energy-to-finish-tasks/solution.py
@@ -0,0 +1,14 @@
+# Approach 1: Greedy (Difference Increasing)
+
+# Time: O(n log n)
+# Space: O(n log n)
+
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        tasks.sort(key = lambda x: x[1] - x[0])
+        ans = 0
+
+        for task in tasks:
+            ans = max(ans + task[0], task[1])
+
+        return ans

3eed6409420687ef0a659b453c91044b77cf56d2
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2026-05-14 06:29:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (89.68%)

---
+++ 2892-check-if-array-is-good/solution.py
@@ -0,0 +1,21 @@
+# Approach 2: Frequency Counting
+
+# Time: O(n)
+# Space: O(n)
+
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        n = len(nums)
+        count = [0] * n
+
+        for a in nums:
+            if a >= n:
+                return False
+            if a < n - 1 and count[a] > 0:
+                return False
+            if a == n - 1 and count[a] > 1:
+                return False
+            count[a] += 1
+
+        return True
+

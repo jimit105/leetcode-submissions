4aad25963b93faa4fa40728c44ecc3d4c5101ea6
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2026-05-26 05:34:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (25.31%)

---
+++ 3408-count-the-number-of-special-characters-i/solution.py
@@ -0,0 +1,6 @@
+# Approach: Hash set
+
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        s = set(word)
+        return sum(c in s and c.upper() in s for c in string.ascii_lowercase)

e3aaff3afbd1c34c7550b105703936ede70223a2
Jimit Dholakia <25223469+jimit105@users.noreply.github.com>
2026-05-29 06:14:52 +0000

    Sync LeetCode submission Runtime - 1 ms (87.03%), Memory - 19.5 MB (21.61%)

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/solution.py
@@ -0,0 +1,13 @@
+# Approach: Math
+
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        ans = float('inf')
+        for num in nums:
+            dig = 0
+            while num > 0:
+                dig += num % 10
+                num //= 10
+            ans = min(ans, dig)
+        return ans
+
